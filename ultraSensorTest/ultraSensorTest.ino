1ed748829453c5912ef72d7eab3d2e8113fbb112
HongN10 <166527122+HongN10@users.noreply.github.com>
2024-10-24 14:44:19 -0400

    uploading code

--- ultraSensorTest/ultraSensorTest.ino
+++ ultraSensorTest/ultraSensorTest.ino
@@ -1,26 +1,87 @@
+#include <WiFiNINA.h>
+#include <ThingSpeak.h>
+
+
+// Ultrasonic sensor pins
+const int trigPin = 2;
+const int echoPin = 3;
+
+
+// WiFi credentials
+const char* ssid = "URI_BME2.4G";
+const char* password = "URIb3m6e1";
+
+WiFiClient client;
+
+// ThingSpeak settings
+unsigned long myChannelNumber = 2711389;
+// const char* myWriteAPIKey = "CAI8XCLH95E9H4C5";
+const char* myWriteAPIKey = "5U24SF1KELOEWI3D";
+
 void setup() {
-  pinMode(3, OUTPUT);//define arduino pin
-  pinMode(2, INPUT);//define arduino pin
-  Serial.begin(9600);//enable serial monitor
+ Serial.begin(9600);
 
+
+ // Sensor pin modes
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+
+
+ // Connect to WiFi
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(1000);
+   Serial.println("Connecting to WiFi...");
+ }
+ Serial.println("Connected to WiFi");
+
+
+ // Initialize ThingSpeak
+ ThingSpeak.begin(client);
+ // while(ThingSpeak.status() != TL_CONNECTED) {
+ //   delay(500);
+ //   Serial.println("Connecting to ThingSpeak...");
+ // }
+ // Serial.println('Connected to ThingSpeak');
 }
+
+
 void loop() {
-  //pulse output
-  digitalWrite(3, LOW);
-  delayMicroseconds(4);
-  digitalWrite(3, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(3, LOW);
-  
-  long t = pulseIn(2, HIGH);//input pulse and save it veriable
-  
-  long inches = t / 74 / 2; //time convert distance
-  long cm = t / 29 / 2; //time convert distance
-  String inch = " inches ";
-  String CM = " cm";
+ // Send a trigger pulse
+ digitalWrite(trigPin, LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
 
-  Serial.println(inches + inch);//print serial monitor inches
-  Serial.println(cm + CM);//print serial monitor cm
-  Serial.println();//print space
-  delay(100);//delay
+
+ // Measure the echo time
+ long duration = pulseIn(echoPin, HIGH);
+ Serial.print("Duration: ");
+ Serial.println(duration);
+
+
+ // Calculate the distance in cm
+ float distance = (duration * 0.0343) / 2;
+
+
+ // Print the distance to the serial monitor
+ Serial.print("Distance: ");
+ Serial.println(distance);
+
+
+ // Send the data to ThingSpeak
+ ThingSpeak.setField(1, distance);  // Set the distance value in field 1
+ int responseCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+
+ if (responseCode == 200) {
+  Serial.println("Data sent to ThingSpeak");
+} else {
+  Serial.println("Failed to send data to ThingSpeak");
 }
+
+
+delay(200);  // Wait 20 seconds
+}
+
